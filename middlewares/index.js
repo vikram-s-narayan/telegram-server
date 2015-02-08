a9d4a63bd44364b481dfd02afddc0b1a8044c258
vikram-s-narayan <vikram@freado.com>
2015-02-08 05:11:20 -0800

    persistent sessions implemented

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,12 +2,17 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');//takes cookies and makes into json objects
 //var passport = require('./middlewares/auth');
 var session = require('express-session')
+var MongoStore = require('connect-mongostore')(session);
 var passport = require('./auth');
 
 exports = module.exports = function (app) {
   app.use(cookieParser());
   app.use(bodyParser.json());
-  app.use(session({ secret: 'apples and oranges', resave: false, saveUninitialized: true }));
+  app.use(session({ secret: 'apples and oranges',
+                    resave: false,
+                    saveUninitialized: true,
+                    store: new MongoStore({'db': 'sessions'})
+                    }));
   app.use(passport.initialize()); //this middleware (function with 3 arguments) ... and part of passport;
   app.use(passport.session());
 }
