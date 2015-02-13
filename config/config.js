e616881349de5c21e5fbb9afe880b38f80fa286c
vikram-s-narayan <vikram@freado.com>
2015-02-12 22:24:22 -0800

    config-prod.json added

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
 var fs    = require('fs');
-var logger = require('nlogger');
+var logger = require('nlogger').logger(module);
 var nconf = require('nconf')
   , path = require('path');
 nconf.env();//loads all variables from unix system and makes available;
 if(nconf.get('NODE_ENV')==='development'){
   nconf.file({ file: path.join(__dirname, 'config-dev.json') });
+  console.log('using config-dev.json');
 } else if(nconf.get('NODE_ENV')==='production'){
   nconf.file({ file: path.join(__dirname, 'config-prod.json') });
+  console.log('using config-prod.json');
 } else {
-  logger.error('set NODE_ENV');//need to fix this ...
+  logger.error('set NODE_ENV');
   process.exit(1);//global object ... calls exit on process object;
 }
 
